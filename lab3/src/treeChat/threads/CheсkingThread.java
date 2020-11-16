98f56157c030210eb6e05dadfbd87bdc9035d1d2
Krokofol <aleksey_lakhansky@mail.ru>
2020-11-16 17:12:39 +0700

    refactoring

--- lab3/src/treeChat/threads/CheсkingThread.java
+++ lab3/src/treeChat/threads/CheсkingThread.java
@@ -8,43 +8,60 @@ import java.net.Socket;
 public class CheсkingThread implements Runnable{
 
     public void run(){
-        int i, j;
         while (true) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            i = 0;
-            for (Long t : Node.time.values()) {
-                if (System.currentTimeMillis() - t > 7000) {
-                    j = 0;
-                    for (Socket socket : Node.time.keySet()) {
-                        if (i == j) {
-                            if (Node.sockets.peek().getInetAddress().toString().equals(socket.getInetAddress().toString()) && Node.sockets.peek().getPort() == socket.getPort()) {
-                                Node.sockets.poll();
-                                if (Node.nextIP != null) {
-                                    System.out.println("Connecting to : " + String.copyValueOf(Node.nextIP.toCharArray(), 1, Node.nextIP.length() - 1) + ":" + Node.nextPort);
-                                    Node.connecting(String.copyValueOf(Node.nextIP.toCharArray(), 1, Node.nextIP.length() - 1), Node.nextPort);
-                                }
-                            }
-                            Node.sockets.remove(socket);
-                            Node.time.remove(socket, t);
-                            break;
-                        }
-                        j++;
-                    }
-                }
-                i++;
-            }
-            if (Node.packets.size() > 200)
-                for (int ii = 0; ii < 50; ii++) {
-                    Node.packets.poll();
-                }
-            if (Node.receivedPackets.size() > 200)
-                for (int ii = 0; ii < 50; ii++) {
-                    Node.receivedPackets.poll();
-                }
+            sleep();
+            checkTime();
+            cleanMessages();
         }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void checkTime() {
+        int i;
+        int j;
+        i = 0;
+        for (Long t : Node.time.values()) {
+            if (System.currentTimeMillis() - t > 7000) {
+                j = 0;
+                for (Socket socket : Node.time.keySet()) {
+                    if (i == j) {
+                        connectToNewHost(socket);
+                        Node.sockets.remove(socket);
+                        Node.time.remove(socket, t);
+                        break;
+                    }
+                    j++;
+                }
+            }
+            i++;
+        }
+    }
+
+    private void connectToNewHost(Socket socket) {
+        if (Node.sockets.peek().getInetAddress().toString().equals(socket.getInetAddress().toString()) && Node.sockets.peek().getPort() == socket.getPort()) {
+            Node.sockets.poll();
+            if (Node.nextIP != null) {
+                System.out.println("Connecting to : " + String.copyValueOf(Node.nextIP.toCharArray(), 1, Node.nextIP.length() - 1) + ":" + Node.nextPort);
+                Node.connecting(String.copyValueOf(Node.nextIP.toCharArray(), 1, Node.nextIP.length() - 1), Node.nextPort);
+            }
+        }
+    }
+
+    private void cleanMessages() {
+        if (Node.packets.size() > 200)
+            for (int ii = 0; ii < 50; ii++) {
+                Node.packets.poll();
+            }
+        if (Node.receivedPackets.size() > 200)
+            for (int ii = 0; ii < 50; ii++) {
+                Node.receivedPackets.poll();
+            }
     }
 }
--- lab3/src/treeChat/threads/ReceiveThread.java
+++ lab3/src/treeChat/threads/ReceiveThread.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.*;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static treeChat.message.MessageType.Chat_MESSAGE;
@@ -19,96 +18,123 @@ public class ReceiveThread implements Runnable{
 
     public void run() {
         while (true) {
-            ConcurrentLinkedQueue<Message> recvPackets = Node.receivedPackets;
             try {
-                //System.out.println("i try");
                 DatagramPacket recvPacket = new DatagramPacket(new byte[2048], 2048);
                 SenderThread.datagramSocket.receive(recvPacket);
                 ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(recvPacket.getData()));
                 Message gotMessage = (Message) ois.readObject();
-                receive(gotMessage, recvPacket);
+                parseMessage(gotMessage, recvPacket);
             } catch (ClassNotFoundException | IOException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    private void receive(Message message, DatagramPacket packet) {
+    private void parseMessage(Message message, DatagramPacket packet) {
         if (Node.nodeInfo.getPercentLoss() > ThreadLocalRandom.current().nextInt(0, 100)) return;
         switch (message.getType()) {
             case Chat_MESSAGE :
-                boolean contains = false;
-                for (Message message1 : Node.receivedPackets) {
-                    if (message1.getUuid().equals(message.getUuid())) {
-                        contains = true;
-                        break;
-                    }
-                }
-                if (contains) {
-                    Message confirmMessage = new Message(MessageType.Confirmation_MESSAGE, Node.nodeInfo.getName(), "accepted", message.getUuid());
-                    for (Socket socket : Node.sockets) {
-                        if (socket.getPort() == packet.getPort()) {
-                            Node.packets.add(new Packet(socket, confirmMessage));
-                            break;
-                        }
-                    }
-                } else {
-                    for (Socket socket : Node.sockets) {
-                        if (socket.getPort() == packet.getPort()) {
-                            Message confirmMessage = new Message(MessageType.Confirmation_MESSAGE, Node.nodeInfo.getName(), "accepted", message.getUuid());
-                            Node.packets.add(new Packet(socket, confirmMessage));
-                        } else {
-                            Message newMessage = new Message(Chat_MESSAGE, message.getName(), message.getText(), UUID.randomUUID());
-                            Node.packets.add(new Packet(socket, newMessage));
-                        }
-                    }
-                    Node.receivedPackets.add(message);
-                    System.out.println(message.getName() + " : " + message.getText());
-                }
+                parseChatMessage(message, packet);
                 break;
             case Confirmation_MESSAGE:
-                Packet packet1 = null;
-                for (Packet packet2 : Node.packets) {
-                    if (packet2.getMessage().getUuid() == message.getUuid()) {
-                        packet1 = packet2;
-                        break;
-                    }
-                }
-                Node.packets.remove(packet1);
+                parseConfirmationMessage(message);
                 break;
             case Ping_MESSAGE:
-                //System.out.println("PING FROM " + packet.getPort());
-                boolean key = false;
-                for (Socket socket : Node.sockets) {
-                    if (socket.getPort() == packet.getPort())  {
-                        key = true;
-                        break;
-                    }
-                }
-                if (!key) {
-                    try {
-                        Socket socket = new Socket(String.copyValueOf(packet.getAddress().toString().toCharArray(), 1, packet.getAddress().toString().length() - 1), packet.getPort());
-                        Node.sockets.add(socket);
-                        Node.time.put(socket, System.currentTimeMillis());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        System.out.println("SDSDSA");
-                        System.exit(404);
-                    }
-                }
-                for (Socket socket : Node.time.keySet()) {
-                    if (socket.getPort() == packet.getPort()) {
-                        Node.time.replace(socket, System.currentTimeMillis());
-                    }
-                }
-                if (!(message.getName().equals("/" + packet.getAddress().getHostAddress()) && Integer.parseInt(message.getText()) == Node.nodeInfo.getPort())) {
-                    Node.nextIP = message.getName();
-                    Node.nextPort = Integer.parseInt(message.getText());
-                } else {
-                    Node.nextIP = null;
-                    Node.nextPort = null;
-                }
+                parsePingMessage(message, packet);
                 break;
         }
+    }
+
+    private void parsePingMessage(Message message, DatagramPacket packet) {
+        boolean key = searchSocket(packet);
+        if (!key) addSocket(packet);
+        updateSocketTimer(packet);
+        parseNextHost(message, packet);
+    }
+
+    private void parseNextHost(Message message, DatagramPacket packet) {
+        if (!(message.getName().equals("/" + packet.getAddress().getHostAddress()) && Integer.parseInt(message.getText()) == Node.nodeInfo.getPort())) {
+            Node.nextIP = message.getName();
+            Node.nextPort = Integer.parseInt(message.getText());
+        } else {
+            Node.nextIP = null;
+            Node.nextPort = null;
+        }
+    }
+
+    private void updateSocketTimer(DatagramPacket packet) {
+        for (Socket socket : Node.time.keySet())
+            if (socket.getPort() == packet.getPort())
+                Node.time.replace(socket, System.currentTimeMillis());
+    }
+
+    private void addSocket(DatagramPacket packet) {
+        try {
+            Socket socket = new Socket(String.copyValueOf(packet.getAddress().toString().toCharArray(), 1, packet.getAddress().toString().length() - 1), packet.getPort());
+            Node.sockets.add(socket);
+            Node.time.put(socket, System.currentTimeMillis());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error : creating socket \n parsePingMessage");
+            System.exit(404);
+        }
+    }
+
+    private boolean searchSocket(DatagramPacket packet) {
+        for (Socket socket : Node.sockets)
+            if (socket.getPort() == packet.getPort())
+                return true;
+        return false;
+    }
+
+    private void parseConfirmationMessage(Message message) {
+        Packet packet1 = null;
+        for (Packet packet2 : Node.packets) {
+            if (packet2.getMessage().getUuid() == message.getUuid()) {
+                packet1 = packet2;
+                break;
+            }
+        }
+        Node.packets.remove(packet1);
+    }
+
+    private void parseChatMessage(Message message, DatagramPacket packet) {
+        boolean contains = searchChatMessage(message);
+        if (contains) {
+            sendConfirmMessage(message, packet);
+        } else {
+            parseNewChatMessage(message, packet);
+        }
+    }
+
+    private void parseNewChatMessage(Message message, DatagramPacket packet) {
+        for (Socket socket : Node.sockets) {
+            if (socket.getPort() == packet.getPort()) {
+                Message confirmMessage = new Message(MessageType.Confirmation_MESSAGE, Node.nodeInfo.getName(), "accepted", message.getUuid());
+                Node.packets.add(new Packet(socket, confirmMessage));
+            } else {
+                Message newMessage = new Message(Chat_MESSAGE, message.getName(), message.getText(), UUID.randomUUID());
+                Node.packets.add(new Packet(socket, newMessage));
+            }
+        }
+        Node.receivedPackets.add(message);
+        System.out.println(message.getName() + " : " + message.getText());
+    }
+
+    private void sendConfirmMessage(Message message, DatagramPacket packet) {
+        Message confirmMessage = new Message(MessageType.Confirmation_MESSAGE, Node.nodeInfo.getName(), "accepted", message.getUuid());
+        for (Socket socket : Node.sockets) {
+            if (socket.getPort() == packet.getPort()) {
+                Node.packets.add(new Packet(socket, confirmMessage));
+                break;
+            }
+        }
+    }
+
+    private boolean searchChatMessage(Message message) {
+        for (Message message1 : Node.receivedPackets)
+            if (message1.getUuid().equals(message.getUuid()))
+                return true;
+        return false;
     }
 }
